74d6ac38516e85cfe957775c7129a8976c459e52
Lydia Amanda Portillo <alchemy@Lydias-MacBook-Pro-8.local>
2016-06-02 14:21:43 -0700

    new changes

---
+++ _js/script.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+	$('.change').click(function () {
+		$("h1").css("color", "purple");
+	});
+
+ 	$(".navbar2").attr("id", "potato");
+
+}); 
+
+$("#icon_link").click(function() {
+	$(".topnav").toggleClass("responsive");
+	});
+
+
