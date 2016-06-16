1fefb1a48f58a418959f3b431a1b9771adaa24b4
Lydia Amanda Portillo <alchemy@Lydias-MacBook-Pro-8.local>
2016-06-16 13:25:24 -0700

    pushing everything

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
-$( document ).ready(function() {
-	$('.change').click(function () {
-		$("h1").css("color", "purple");
-	});
+// $( document ).ready(function() {
+// 	$('.change').click(function () {
+// 		$("h1").css("color", "purple");
+// 	});
 
- 	$(".navbar2").attr("id", "potato");
+//  	$(".navbar2").attr("id", "potato");
 
-}); 
+// }); 
 
-$("#icon_link").click(function() {
-	$(".topnav").toggleClass("responsive");
-	});
+// $("#icon_link").click(function() {
+// 	$(".topnav").toggleClass("responsive");
+// 	});
 
+
+$(document).ready(function() {
+    $(document).foundation();
+});
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2000); // Change image every 3 seconds
+}
 
